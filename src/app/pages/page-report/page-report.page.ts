8231dc38d18a9cdba5aba733b677a6f5ef287544
Book <nitiphat230740@gmail.com>
2019-11-14 03:17:28 +0700

    no message

--- src/app/pages/page-report/page-report.page.ts
+++ src/app/pages/page-report/page-report.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-report',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageReportPage implements OnInit {
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
   }
-
+  async logout(){
+    try {
+      await this.authService.logoutUser();
+      this.router.navigateByUrl('login');
+    } catch (error) { }
+  }
 }
